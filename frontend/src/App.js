c54ab0a3b8026ed73d6e885f5d255e15ab021d13
Charles Gabriel Carvalho Cristovão <charlesgabriel10@gmail.com>
2020-03-27 19:21:59 -0300

    Atualizando frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './global.css'
+
+//import Routes from './routes';
+import Logon from './pages/Logon';
+
+function App() {
+  return (
+    <Logon />
+  );
+}
+
+export default App;
